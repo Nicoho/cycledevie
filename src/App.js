9d1af5ae719b3d0c799a5b4d9d6a978314b19079
Nicolas Hosteins <n.hosteins@gmail.com>
2019-04-28 19:28:45 +0200

    reponse

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor (props){
+    super (props);
+    this.state={  
+      title:"",
+      globalTitle:""
+    }
+  }
+
+  componentDidMount (){
+    console.log('Formulaire rendu')
+  }
+    
+  onChange = (e)=>{
+    this.setState({title:e.target.value})
+  }
+  
+  getSubmit = (e) => {
+    e.preventDefault()
+    this.setState({globalTitle:`Mon Formlaire - ${this.state.title}`})
+  }
+
+  componentDidUpdate(){
+    console.log('titre changé')
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <form onSubmit={this.getSubmit}>
+          <input type="text" 
+                name='text' 
+                id='text'
+                value={this.state.title}
+                onChange={this.onChange}
+                />
+                
+
+          <button> cliquez</button>
+        </form>
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
